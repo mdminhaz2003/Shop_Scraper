deae14ce675809199b2932c988dc9fb7847d099c
MD. MINHAZ <mdm047767@gmail.com>
2022-06-27 19:19:58 +0600

    Update third.py

--- third.py
+++ third.py
@@ -4,12 +4,11 @@ from tinydb import TinyDB
 db = TinyDB('db2.json')
 db_data = db.all()
 headers = [key for key in db_data[0].keys()]
-
+value = headers.index("Option1_Value")
 header_text = ""
 for header in headers:
-    header_text = header if header_text == "" else f"{header_text},{header}"
+    header_text += f"{header},"
 header_text += "\n"
-
 with open("product_info_spotify_format.csv", "w") as file:
     file.write(header_text)
     for single_row_data in db_data:
@@ -34,18 +33,17 @@ with open("product_info_spotify_format.csv", "w") as file:
             for line in range(number_of_line - 1):
                 secondary_line_data_list = ["" for value in headers]
 
-                size_index = headers.index("Option1_Value") - 1
-                color_index = headers.index("Option2_Value") - 1
-                price_index = headers.index("Variant_Price") - 1
-                img_link_index = headers.index("Original_Image_Src") - 1
-                img_position_index = headers.index("Image_Position") - 1
+                size_index = headers.index("Option1_Value")
+                color_index = headers.index("Option2_Value")
+                price_index = headers.index("Variant_Price")
+                img_link_index = headers.index("Original_Image_Src")
+                img_position_index = headers.index("Image_Position")
 
                 secondary_line_data_list[size_index] = single_row_data["Option1_Value"][value_index]
                 secondary_line_data_list[color_index] = single_row_data["Option2_Value"][value_index]
                 secondary_line_data_list[price_index] = single_row_data["Variant_Price"]
                 secondary_line_data_list[img_link_index] = single_row_data["Original_Image_Src"][value_index]
                 secondary_line_data_list[img_position_index] = single_row_data["Image_Position"][value_index]
-
                 single_row_data_list.append(secondary_line_data_list)
                 value_index += 1
         else:
@@ -54,7 +52,7 @@ with open("product_info_spotify_format.csv", "w") as file:
         single_line_text = ""
         for line in single_row_data_list:
             for word in line:
-                single_line_text += word if single_line_text == "" else f",{word}"
+                single_line_text += f"{word},"
             single_line_text += "\n"
         file.write(single_line_text)
 
