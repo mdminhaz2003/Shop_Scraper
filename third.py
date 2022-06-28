ebe0c3db1dd985dc3e63b592639ac737553c7a31
MD. MINHAZ <mdm047767@gmail.com>
2022-06-29 01:44:28 +0600

    Update third.py

--- third.py
+++ third.py
@@ -35,12 +35,18 @@ with open("product_info_spotify_format.csv", "w") as file:
 
                 size_index = headers.index("Option1_Value")
                 color_index = headers.index("Option2_Value")
+                rgb_color = headers.index("Option3_Value")
+                product_key = headers.index("Option4_Value")
+                color_code = headers.index("Option5_Value")
                 price_index = headers.index("Variant_Price")
                 img_link_index = headers.index("Original_Image_Src")
                 img_position_index = headers.index("Image_Position")
 
                 secondary_line_data_list[size_index] = single_row_data["Option1_Value"][value_index]
                 secondary_line_data_list[color_index] = single_row_data["Option2_Value"][value_index]
+                secondary_line_data_list[rgb_color] = single_row_data["Option3_Value"][value_index]
+                secondary_line_data_list[product_key] = single_row_data["Option4_Value"][value_index]
+                secondary_line_data_list[color_code] = single_row_data["Option5_Value"][value_index]
                 secondary_line_data_list[price_index] = single_row_data["Variant_Price"]
                 secondary_line_data_list[img_link_index] = single_row_data["Original_Image_Src"][value_index]
                 secondary_line_data_list[img_position_index] = single_row_data["Image_Position"][value_index]
